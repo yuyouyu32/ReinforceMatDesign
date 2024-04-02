6cfd98af692fe2bba727d22a660dfde97107f020
yuyouyu32 <624297803@qq.com>
2024-04-02 19:05:36 +0800

    add: add similarity grid search result

--- src/methods/grid_search.py
+++ src/methods/grid_search.py
@@ -26,15 +26,18 @@ if __name__ == '__main__':
     result_df = pd.read_excel('../results/random_search_result.xlsx')
     # 筛选 Dmax(mm): 5-20 ; yield(MPa): 1500-2000; E(%):10-35%的数据
     result_df = result_df[(result_df['Dmax(mm)'] >= 5) & (result_df['Dmax(mm)'] <= 20) & (result_df['yield(MPa)'] >= 1500) & (result_df['yield(MPa)'] <= 2200) & (result_df['Ε(%)'] >= 10) & (result_df['Ε(%)'] <= 35)]
-    result_df.to_excel('../results/random_search_result_filtered.xlsx')
+    result_df.to_excel('../results/random_search_result_filtered.xlsx', index=False)
     # 找到每一行result_df 中compositions列与data_path的compositions列之差的sum值最小的3行的index,组成一个数组放到result_df的新列 similar_index中,注意我要找3行
     # 循环result_df中的每一行   
-    original_data = pd.read_excel(data_path).drop(columns = drop_columns).drop(columns = target_columns)
+    original_data = pd.read_excel(data_path)
+    original_features = original_data.drop(columns = drop_columns).drop(columns = target_columns)
     for index, row in result_df.iterrows():
         # 计算差值
-        diff = row[compositions].to_numpy() - original_data.to_numpy()
+        diff = row[compositions].to_numpy() - original_features.to_numpy()
         diff = np.sum(np.abs(diff), axis=1)
         # 找到最小的3个index
         similar_index = np.argsort(diff)[:3]
-        result_df.loc[index, 'similar_index'] = str(similar_index)
-    result_df.to_excel('../results/random_search_result_filtered_similar.xlsx')
+        # 将这3个index根据target_columns的列名和值 还有BMGs列的值拼接成一个字符串
+        similar_BMGs = [original_data.loc[i, 'BMGs'] + ' ' + ' '.join([f'{column}:{original_data.loc[i, column]}' for column in target_columns]) for i in similar_index]
+        result_df.loc[index, 'similar_index'] = '\n'.join(similar_BMGs)
+    result_df.to_excel('../results/random_search_result_filtered_similar.xlsx', index=False)
