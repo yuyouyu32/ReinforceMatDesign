7c6ee965f790395e0fa6d426e2ba722d6ddb54d6
yuyouyu32 <624297803@qq.com>
2024-04-11 10:38:55 +0800

    add: add score result from GPT

---
+++ src/evaluate/evalue.py
@@ -0,0 +1,99 @@
+import ast
+import json
+import os
+import random
+import re
+import time
+
+import pandas as pd
+from evaluate.prompts import *
+from openai import OpenAI
+from tqdm import tqdm
+
+
+import time
+MaxRetries = 10
+Delay = 15
+
+
+def retry_on_failure(max_retries: int = 3, delay: int = 1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            retries = 0
+            while retries < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    retries += 1
+                    print(f"Attempt {retries}/{max_retries} failed: {e}")
+                    time.sleep(delay)
+            raise Exception(f"Failed after {max_retries} attempts")
+        return wrapper
+    return decorator
+
+
+def calculate_token_price(prompt_tokens, completion_tokens):
+    return 7.13 * (0.01 * prompt_tokens / 1000 + 0.03 * completion_tokens / 1000)
+
+@retry_on_failure(max_retries= MaxRetries, delay=Delay)
+def get_rsp_from_GPT(sys_prompt: str, user_prompt: str, n: int = 1):
+    """
+    Description: 
+        Get copywriter from GPT-4
+    Args:
+        sys_prompt (str): System prompt
+        user_prompt (str): User prompt
+        n (int, optional): Number of rsp to generate. Defaults to 1.
+    Returns:
+        rsp (list): List of rsp
+        cost (float): Cost price(￥) of generating rsp
+    """
+    client = OpenAI(base_url=os.environ.get('OPENAI_BASE_URL'), api_key=os.environ.get('OPENAI_API_KEY'))
+    messages = [{'role': 'system', 'content': sys_prompt}, {'role': 'user', 'content': user_prompt}]
+    chat_completion = client.chat.completions.create(messages=messages, model='gpt-4-turbo', n=n)
+    rsps = []
+    for i in range(n):
+        response = chat_completion.choices[i].message.content
+        rsps.append(response)
+    cost = calculate_token_price(chat_completion.usage.prompt_tokens, chat_completion.usage.completion_tokens)
+    return rsps, cost
+
+def parse_result(rsp):
+    try:
+        score_match = re.search(r'"score": (\d+\.\d+)', rsp)
+        score_value = float(score_match.group(1)) if score_match else None
+        return score_value
+    except:
+        return None
+
+def main():
+    all_cost = 0
+    data = pd.read_excel('../results/random_search_result_filtered_similar_score.xlsx')
+    data = data[['BMGs', 'Tg(K)', 'Tx(K)', 'Tl(K)', 'Dmax(mm)', 'yield(MPa)', 'Modulus (GPa)', 'Ε(%)',  'similar_index']]
+    # data 按照 'Dmax(mm)', 'yield(MPa)', 'Modulus (GPa)', 'Ε(%)'的和排序，降序
+    data['Sum'] = data[['Dmax(mm)', 'yield(MPa)', 'Modulus (GPa)', 'Ε(%)']].sum(axis=1)
+    data = data.sort_values(by='Sum', ascending=False)
+    data = data.drop(columns=['Sum'])
+    data.reset_index(drop=True, inplace=True)
+    for index, row in tqdm(data.iterrows(), total=data.shape[0]):
+        if index > 200:
+            break
+        similarities = row['similar_index']
+        data_prompt = str(row.drop('similar_index').to_dict())
+        task_prompt = EvalueUser.format(rule=Rules, data=data_prompt, sim=similarities)
+        rsps, cost = get_rsp_from_GPT(sys_prompt=EvalueSystem, user_prompt=task_prompt, n=3)
+        all_cost += cost
+        scores = []
+        for i, rsp in enumerate(rsps):
+            data.loc[index, f'rsp_{i}'] = rsp
+            score = parse_result(rsp)
+            if  score is not None:
+                scores.append(score)
+        if len(scores) > 0:
+            data.loc[index, 'score'] = round(sum(scores) / len(scores), 2)
+        data.to_excel('../results/random_search_result_filtered_similar_score.xlsx', index=False)
+        time.sleep(random.randint(2, 3))
+    print('Cost: ', all_cost)
+    
+if __name__ == '__main__':
+    main()
---
+++ src/evaluate/prompts.py
@@ -0,0 +1,29 @@
+Rules = """- Compositions with a calculated average atomic size difference greater than 12% across all constituent elements score high, as they are more likely to form amorphous structures.
+- Alloys with five or more principal elements, each constituting between 5% and 35% of the total composition, indicating a high mixing entropy, score highest.
+- Compositions with a majority of element pairs having a negative heat of mixing score higher, as they are more likely to form a stable amorphous phase.
+- Compositions with a high modulus contrast among constituents, particularly with elements known for high Young's modulus, e.g., Ti and some rare earths like Gd, score higher.
+- Alloys with a Tg/Tl ratio closer to or greater than 0.6 score highest, as they demonstrate better thermal stability.
+-  Compositions with a balanced ratio of late transition metals, particularly when combined with Zr or Ti, score higher, as these are conducive to forming amorphous structures.
+- Alloys with a greater variety of elements, especially those that introduce complexity without significantly increasing the tendency for crystalline phase formations, score higher.
+"""
+
+EvalueSystem = "You will act as an expert in materials science, specializing in bulk metallic glass (BMG). Your task is to analyze provided data points, which are based on specific screening rules for HEA components."
+
+EvalueUser = """Firstly, understand and apply the following screening rules defined as RULE. These rules are crucial for evaluating the potential of each HEA component.I will also provide you with performance data for Real BMGs similar to the data in the DATA, which are actually measured and can be used as a reference for your evaluation.
+
+Secondly, evaluate each data point in DATA by assigning a score from 0 to 1, indicating its suitability for experimental validation in bulk metallic glass (BMG). A score of 1 signifies high relevance. Include a short explanation for each score, focusing on pertinent scientific concepts and considering the complexities and potential results of the experimental process in HEAs.
+
+RULE:
+{rule}
+
+Real BMGs:
+{sim}
+
+DATA:
+{data}
+
+Ensure the output for each evaluated data point is formatted as follows, data calculations can be analyzed if necessary. The format of the rating needs to be output in the following format, additional calculations are allowed, but the rating information needs to be wrapped in `{{}}`:
+{{
+    "score": [assigned value],
+    "reason": [brief overview explaining the assigned value]
+}}"""
