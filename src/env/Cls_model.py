be94871a68a7cdae1c5fd3e56e74644637bb1f12
yuyouyu32 <624297803@qq.com>
2024-06-16 23:01:36 +0800

    add: add y pred result of reg and cls

--- src/env/Cls_model.py
+++ src/env/Cls_model.py
@@ -1,13 +1,21 @@
 import glob
+import os
+import sys
+import warnings
 
+import numpy as np
 import pandas as pd
-# from catboost import CatBoostClassifier
-# from lightgbm import LGBMClassifier
+from imblearn.over_sampling import SMOTE
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
 from sklearn.discriminant_analysis import (LinearDiscriminantAnalysis,
                                            QuadraticDiscriminantAnalysis)
 from sklearn.ensemble import (AdaBoostClassifier, GradientBoostingClassifier,
                               RandomForestClassifier)
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (accuracy_score, f1_score, precision_score,
+                             recall_score, roc_auc_score)
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
@@ -17,9 +25,17 @@ from xgboost import XGBClassifier
 
 from config import logging
 from dataloader.my_dataloader import CustomDataLoader
-from imblearn.over_sampling import SMOTE
+
 
 logger = logging.getLogger(__name__)
+
+
+def eval_none(best_param):
+    best_param = eval(best_param)
+    for key, value in best_param.items():
+        if value == 'none':
+            best_param[key] = None
+    return best_param
 
 class Cls_Model:
     def __init__(self, data_path, drop_columns, target_columns, results_path):
@@ -28,7 +44,6 @@ class Cls_Model:
         self.results_path = results_path
 
     def get_best_models(self, norm_features=True):
-
         cls_model = self._get_best_model(self.results_path + '/cls_label_ml.xlsx')
         x, y = self.data.get_features_for_target(self.target_columns[0])
         x = x.to_numpy()
@@ -48,47 +63,124 @@ class Cls_Model:
         model_name = df['auc'].astype(float).idxmax()
         df_dict =  df.to_dict()
         best_param = df_dict['best_params'][model_name]
-
         if model_name == 'LogisticRegression':
-            model = LogisticRegression(**eval(best_param))
+            model = LogisticRegression(**eval_none(best_param))
         elif model_name == 'SVC':
-            model = SVC(**eval(best_param))
+            model = SVC(**eval_none(best_param))
         elif model_name == 'RandomForestClassifier':
-            model = RandomForestClassifier(**eval(best_param))
+            model = RandomForestClassifier(**eval_none(best_param))
         elif model_name == 'GradientBoostingClassifier':
-            model = GradientBoostingClassifier(**eval(best_param))
+            model = GradientBoostingClassifier(**eval_none(best_param))
         elif model_name == 'AdaBoostClassifier':
-            model = AdaBoostClassifier(**eval(best_param))
+            model = AdaBoostClassifier(**eval_none(best_param))
         elif model_name == 'KNeighborsClassifier':
-            model = KNeighborsClassifier(**eval(best_param))
+            model = KNeighborsClassifier(**eval_none(best_param))
         elif model_name == 'XGBClassifier':
-            model = XGBClassifier(**eval(best_param))
+            model = XGBClassifier(**eval_none(best_param))
         elif model_name == 'DecisionTreeClassifier':
-            model = DecisionTreeClassifier(**eval(best_param))
+            model = DecisionTreeClassifier(**eval_none(best_param))
         elif model_name == 'GaussianNB':
-            model = GaussianNB(**eval(best_param))
+            model = GaussianNB(**eval_none(best_param))
         elif model_name == 'MultinomialNB':
-            model = MultinomialNB(**eval(best_param))
+            model = MultinomialNB(**eval_none(best_param))
         elif model_name == 'BernoulliNB':
-            model = BernoulliNB(**eval(best_param))
+            model = BernoulliNB(**eval_none(best_param))
         elif model_name == 'QuadraticDiscriminantAnalysis':
-            model = QuadraticDiscriminantAnalysis(**eval(best_param))
-        # elif model_name == 'CatBoostClassifier':
-            # model = CatBoostClassifier(**eval(best_param))
+            model = QuadraticDiscriminantAnalysis(**eval_none(best_param))
+        elif model_name == 'CatBoostClassifier':
+            model = CatBoostClassifier(**eval_none(best_param))
         else:
             raise ValueError(f"Model {model_name} is not recognized.")
 
         return model
 
+    
+    def get_all_models(self, model_path):
+        df = pd.read_excel(model_path, index_col=0).T
+        df_dict = df.to_dict()
+        all_models = {}
+        for model_name in df_dict['auc'].keys():
+            best_param = df_dict['best_params'][model_name]
+
+            if model_name == 'LogisticRegression':
+                all_models[model_name] = LogisticRegression(**eval_none(best_param))
+            elif model_name == 'SVC':
+                best_param = eval_none(best_param)
+                best_param['probability'] = True
+                all_models[model_name] = SVC(**best_param)     
+            elif model_name == 'RandomForestClassifier':
+                all_models[model_name] = RandomForestClassifier(**eval_none(best_param))
+            elif model_name == 'GradientBoostingClassifier':
+                all_models[model_name] = GradientBoostingClassifier(**eval_none(best_param))
+            elif model_name == 'AdaBoostClassifier':
+                all_models[model_name] = AdaBoostClassifier(**eval_none(best_param))
+            elif model_name == 'KNeighborsClassifier':
+                all_models[model_name] = KNeighborsClassifier(**eval_none(best_param))
+            elif model_name == 'XGBClassifier':
+                all_models[model_name] = XGBClassifier(**eval_none(best_param))
+            elif model_name == 'DecisionTreeClassifier':
+                all_models[model_name] = DecisionTreeClassifier(**eval_none(best_param))
+            elif model_name == 'GaussianNB':
+                all_models[model_name] = GaussianNB(**eval_none(best_param))
+            elif model_name == 'MultinomialNB':
+                all_models[model_name] = MultinomialNB(**eval_none(best_param))
+            elif model_name == 'BernoulliNB':
+                all_models[model_name] = BernoulliNB(**eval_none(best_param))
+            elif model_name == 'QuadraticDiscriminantAnalysis':
+                all_models[model_name] = QuadraticDiscriminantAnalysis(**eval_none(best_param))
+            elif model_name == 'CatBoostClassifier':
+                all_models[model_name] = CatBoostClassifier(**eval_none(best_param))
+            else:
+                raise ValueError(f"Model {model_name} is not recognized.")
+        return all_models
+            
+    
+    def eval_model(self, model, features, target):
+        y_tests = []
+        y_pred_probas = []
+        kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+        for train_index, test_index in kf.split(features, target):
+            X_train, X_test = features[train_index], features[test_index]
+            y_train, y_test = target[train_index], target[test_index]
+            model.fit(X_train, y_train)
+            y_pred_proba = model.predict_proba(X_test)[:, 1]
+            y_tests.extend(y_test)
+            y_pred_probas.extend(y_pred_proba)
+        return y_tests, y_pred_probas
+
+    def get_cls_results(self, norm_features=True, save_path="../results/cls_pred_results"):
+        x, y = self.data.get_features_for_target(self.target_columns[0])
+        x = x.to_numpy()
+        smote = SMOTE(random_state=42)
+        x, y = smote.fit_resample(x, y)
+        if norm_features:
+            x_sums = x.sum(axis=1, keepdims=True)
+            x = x / x_sums
+        
+        all_models = self.get_all_models(self.results_path + '/cls_label_ml.xlsx')
+        save_df = pd.DataFrame()
+        for model_name, model in all_models.items():
+            logger.info(f"Evaluating {model_name} with K-fold cross-validation...")
+            y_tests, y_pred_probas = self.eval_model(model, x, y)
+            save_df[model_name] = y_pred_probas
+            if 'y_test' not in save_df.columns:
+                save_df['y_test'] = y_tests
+            else:
+                for i, y_label in enumerate(y_tests):
+                    assert y_label == save_df['y_test'][i]
+            logger.info(f"Model {model_name} AUC: {roc_auc_score(y_tests, y_pred_probas)}")
+        save_df.to_excel(f"{save_path}/cls_y_pred_proba.xlsx", index=False)
+            
+            
 def unit_test():
-    data_path = '/data/home/yeyongyu/SHU/ReinforceMatDesign/data/ALL_data_cls.xlsx'  # Replace with your file path
+    data_path = '../data/ALL_data_cls.xlsx'  # Replace with your file path
     drop_columns = ['Class', 'GFA', "Chemical composition"]
     target_columns = ['cls_label']
-    results_path = '/data/home/yeyongyu/SHU/ReinforceMatDesign/results/Cls'
+    results_path = '../results/Cls'
 
     ml_model = Cls_Model(data_path, drop_columns, target_columns, results_path)
-    best_models = ml_model.get_best_models()
-    # print(best_models.predict_proba(x[10:20]))
+    ml_model.get_cls_results()
 
+# python -m env.Cls_model
 if __name__ == '__main__':
     unit_test()
--- src/env/ML_model.py
+++ src/env/ML_model.py
@@ -1,9 +1,12 @@
 import glob
 
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import (AdaBoostRegressor, GradientBoostingRegressor,
                               RandomForestRegressor)
 from sklearn.linear_model import ElasticNet, Lasso, Ridge
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from xgboost import XGBRegressor
@@ -19,6 +22,25 @@ class ML_Model:
         self.data = CustomDataLoader(data_path, drop_columns, target_columns)
         self.target_columns = target_columns
         self.results_path = results_path
+        self.minmax_record = {}
+        for target_clomuns in target_columns:
+            self.minmax_record[target_clomuns] = self.data.data[target_clomuns].min(), self.data.data[target_clomuns].max()
+
+    def inverse_normal_targets(self, target_name, target_value):
+        """
+        Inverse the normalization of the target value.
+        
+        Parameters:
+            target_name (str): target name
+            target_value (float): target value
+        
+        Returns:
+            float: inverse normalized target value
+        """
+        # df[target_columns] = df[target_columns].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+        min_value, max_value = self.minmax_record[target_name]
+        return target_value * (max_value - min_value) + min_value
+    
     
     def get_best_models(self, norm_features=True, norm_target=True):
         xlsx_paths = glob.glob(self.results_path + '/*.xlsx')
@@ -71,15 +93,69 @@ class ML_Model:
             raise ValueError("Unknown model name.")
         
         return model
+    
+    def get_pred_result(self, norm_features=True, norm_target=True, save_path="../results/pred_results"):
+        xlsx_paths = glob.glob(self.results_path + '/*.xlsx')
+        best_models = {}
+        for target_column in self.target_columns:
+            # Find the best model for each target column
+            for xlsx_path in xlsx_paths:
+                if target_column in xlsx_path:
+                    best_models[target_column] = self._get_best_model(xlsx_path)
+                    x, y = self.data.get_features_for_target(target_column)
+                    x = x.to_numpy()
+                    if norm_features:
+                        x_sums = x.sum(axis=1, keepdims=True)
+                        x = x / x_sums
+                    y = y.to_numpy()
+                    if norm_target:
+                        y = (y - y.min()) / (y.max() - y.min())
+                    y_pred, y_test = self.eval_model(best_models[target_column], x, y, target_column)
+                    save_df = pd.DataFrame({'y_pred': y_pred, 'y_test': y_test})
+                    save_df.to_excel(f"{save_path}/{target_column}.xlsx", index=False)
+                    logger.info(f"Prediction results for {target_column} saved to {save_path}/{target_column}.xlsx")     
+                            
+    def eval_model(self, model, features, target, target_name):
+        r2_scores = []
+        rmse_scores = []
+        mape_scores = []
+        y_preds = []
+        y_tests = []
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
+        print(f"Target {target_name} Model: {model.__class__.__name__} len(features): {len(features)}")
+        for train_index, test_index in kf.split(features):
+            X_train, X_test = features[train_index], features[test_index]
+            y_train, y_test = target[train_index], target[test_index]
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2_scores.append(r2_score(y_test, y_pred))
+            for i, y in enumerate(y_test):
+                y_test[i] = self.inverse_normal_targets(target_name, y)
+            y_tests.extend(y_test)
+            for i, y in enumerate(y_pred):
+                y_pred[i] = self.inverse_normal_targets(target_name, y)
+            y_preds.extend(y_pred)
+            rmse_scores.append(np.sqrt(mean_squared_error(y_test, y_pred)))
+            # Handling zero values in target for MAPE calculation
+            y_test_without_zeros = np.where(y_test != 0, y_test, 1e-8)
+            y_pred_without_zeros = np.where(y_test != 0, y_pred, 1e-8)
+            mape = np.mean(np.abs((y_test_without_zeros - y_pred_without_zeros) / y_test_without_zeros)) * 100
+            mape_scores.append(mape)
+        mean_r2_score = np.mean(r2_scores)
+        mean_rmse_score = np.mean(rmse_scores)
+        mean_mape_score = np.mean(mape_scores)
+        logger.info(f"Target {target_name} Model: {model.__class__.__name__} R2 Score: {mean_r2_score}, RMSE: {mean_rmse_score}, MAPE: {mean_mape_score}")
+        return y_preds, y_tests
 
 def unit_test():
-    data_path = '/Users/yuyouyu/WorkSpace/Mine/ReinforceMatDesign/data/ALL_data_grouped_processed.xlsx'  # Replace with your file path
+    data_path = '../data/ALL_data_grouped_processed.xlsx'  # Replace with your file path
     drop_columns = ['BMGs', "Chemical composition"]
     target_columns = ['Tg(K)', 'Tx(K)', 'Tl(K)', 'Dmax(mm)', 'yield(MPa)', 'Modulus (GPa)', 'Ε(%)']
-    results_path = '/Users/yuyouyu/WorkSpace/Mine/ReinforceMatDesign/results/ML_All'
+    results_path = '../results/ML_All'
 
     ml_model = ML_Model(data_path, drop_columns, target_columns, results_path)
-    best_models = ml_model.get_best_models()
+    ml_model.get_pred_result()
 
+# python -m env.ML_model
 if __name__ == '__main__':
     unit_test()
